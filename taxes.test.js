c9760faa3a70299511c0cf306b75f501b65599dd
Alex Schmidt <AlexSchmidt@alexs-mbp.mynetworksettings.com>
2022-09-28 16:47:36 -0400

    committing all 3

--- taxes.test.js
+++ taxes.test.js
@@ -1,12 +1,14 @@
-it("should calculate lower-bracket taxes", function () {
-	expect(calculateTaxes(10000)).toEqual(1500);
-	expect(calculateTaxes(20000)).toEqual(3000);
-});
+describe("Calculating tax brackets", function () {
+	it("should calculate lower-bracket taxes", function () {
+		expect(calculateTaxes(10000)).toEqual(1500);
+		expect(calculateTaxes(20000)).toEqual(3000);
+	});
 
-it("should calculate higher-bracket taxes", function () {
-	expect(calculateTaxes(50000)).toEqual(12500);
-	expect(calculateTaxes(80000)).toEqual(20000);
-	expect(calculateTaxes(0)).toEqual(0);
+	it("should calculate higher-bracket taxes", function () {
+		expect(calculateTaxes(50000)).toEqual(12500);
+		expect(calculateTaxes(80000)).toEqual(20000);
+		expect(calculateTaxes(0)).toEqual(0);
+	});
 });
 
 it("should reject error incomes", function () {
